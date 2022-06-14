12ab3e3d1d6e11f4c17cb51ac78e8487e63c2b2d
irfanalibot0 <104135320+irfanalibot0@users.noreply.github.com>
2022-06-14 16:44:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Owner", "https://t.me/Irfan_Ali_786")
+    buttons.buildbutton("Update Channel", "https://t.me/X_BOT_UPDATES")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
